1f41ea9e159a94f330e90141902285ac64c54d47
soufiane <soufian9mkns@gmail.com>
2023-11-19 01:54:25 -0800

    inserting algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *pointer, *ash, *temp;
+
+	if(list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	ash = (*list)->next;
+
+	while (ash != NULL)
+	{
+		temp = ash;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			pointer = temp->prev;
+			if (temp ->next != NULL)
+				temp->next->prev = pointer;
+			if(pointer->prev != NULL)
+				pointer->prev->next = temp;
+			else
+				*list = temp;
+			temp->prev = pointer->prev;
+			pointer->next = temp->next;
+			temp->next = pointer;
+			pointer->prev = temp;
+			temp = pointer->prev;
+			print_list(*list);
+		}
+		ash = ash->next;
+	}
+}
