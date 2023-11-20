b7833a980298841fd88769ee799d33f06ec6a81f
Unique-Usman <usmanakinyemi202@gmail.com>
2023-11-20 23:20:59 +0530

    Added the implementation for quick sort algorithm and also remove binaries from the repo

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
-#ifndef SORT
-#define SORT
-
+#ifndef SORT_H
+#define SORT_H
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,9 +18,12 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-void bubble_sort(int *array, size_t size);
+
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-#endif /*SORT*/
+void quick_sort(int *array, size_t size);
+
+#endif
