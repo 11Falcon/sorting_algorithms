07a22df806b6e3b5259ce9ad61a09306e86b3595
soufiane <soufian9mkns@gmail.com>
2023-11-21 13:29:55 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,31 +10,29 @@
  */
 int lomuto_partition(int *array, int low, int high, int size)
 {
-    int pivot, i, temp, j;
-
-    pivot = array[high];
-    i = low - 1;
-
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            if (i != j)
-                print_array(array, size);
-        }
-    }
-
-    temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
-    if (i + 1 != high)
-        print_array(array, size);
-
-    return (i + 1);
+	int pivot, i, temp, j;
+	
+	pivot = array[high];
+	i = low - 1;
+	
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	if (i + 1 != high)
+		print_array(array, size);
+	return (i + 1);
 }
 
 /**
@@ -46,15 +44,14 @@ int lomuto_partition(int *array, int low, int high, int size)
  */
 void sort(int *array, int low, int high, int size)
 {
-    int pivot_index;
+	int pivot_index;
 
-    if (low < high)
-    {
-        pivot_index = lomuto_partition(array, low, high, size);
-
-        sort(array, low, pivot_index - 1, size);
-        sort(array, pivot_index + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+		sort(array, low, pivot_index - 1, size);
+		sort(array, pivot_index + 1, high, size);
+	}
 }
 
 /**
@@ -64,8 +61,7 @@ void sort(int *array, int low, int high, int size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    sort(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	sort(array, 0, size - 1, size);
 }
-
